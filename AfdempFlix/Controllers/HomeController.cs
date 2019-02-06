31cd3d47d9debf7079d6183662afad36afc7af92
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-02-06 21:12:45 +0200

    fixed Home Layout

--- AfdempFlix/Controllers/HomeController.cs
+++ AfdempFlix/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AfdempFlix.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace AfdempFlix.Controllers
 {
     public class HomeController : Controller
     {
+        private MoviesContext db = new MoviesContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.MyEntities.ToList());
         }
 
         public ActionResult About()
